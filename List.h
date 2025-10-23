77d060d2aaa52c0da70c909520bebb67c6b52532
abaghov <abaghov@upv.edu.es>
2025-10-23 17:36:11 +0200

    Practica 1 de la asignatura de Programación Avanzada

--- List.h
+++ List.h
@@ -1,19 +1,18 @@
 #ifndef LIST_H
 #define LIST_H
 
-using namespace std;
 
 template <typename T>
-class LIST {
+class List {
 	public:
-		virtual void insert(int pos, T e);
-		virtual void append(T e);
-		virtual void prepend(T e);
-		virtual T remove(int pos);
-		virtual T get(int pos);
-		virtual int search(T e);
-		virtual bool empty();
-		virtual int size();
+		virtual void insert(int pos, T e) = 0;
+		virtual void append(T e) = 0;
+		virtual void prepend(T e) = 0;
+		virtual T remove(int pos) = 0;
+		virtual T get(int pos) = 0;
+		virtual int search(T e) = 0;
+		virtual bool empty() = 0;
+		virtual int size() = 0;
 			
 };
 
