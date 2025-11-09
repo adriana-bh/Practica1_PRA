43e514e104ad0eb8e7231e4858663227f531ee48
abaghov <abaghov@upv.edu.es>
2025-11-09 17:46:36 +0100

    Añadido código de test de la clase ListLinked; Makefile actualizado

--- ListLinked.h
+++ ListLinked.h
@@ -1,157 +1,190 @@
-#include <ostream>
 #include "List.h"
 #include "Node.h"
+#include <iostream>
 
 template <typename T>
 class ListLinked : public List<T> {
 
-	private:
-		Node<T>* first;
-		int n;
+    private:
+ 
+     	Node<T>* first;
+	int n;
 
-	public:
-		ListLinked(){
-			first = nullptr;
-			n = 0;
-		}
+    public:
+	
+	ListLinked(){
 
-		~ListLinked(){
-			Node<T>* aux = first;
-			while (aux != nullptr){
-				Node<T>* eliminar = first;
-				first = first -> next;
-				aux = aux -> next;
-				delete eliminar;
-			}
-			first = nullptr;
-			n = 0;
-		}
+	  this->n = 0;
+	  first = nullptr;
+	}
 
-		T operator[](int pos){
-			if(pos <  0 || pos > n-1){
-                        	throw std::out_of_range("La posicion no es valida");
-               		}
-			Node<T>*aux = first;
-                        int i = 0;
-                        while (aux != nullptr && i < pos){
-                                aux = aux -> next;
-                                i++;
-                        }
-                        T elem = aux->data;
-                        return elem;
-		}
+	
+	~ListLinked(){
 
-		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
-			Node<T>*aux = list.first;
-			while(aux != nullptr){
-				out << aux->data<< " "<<std::endl;
-				aux = aux -> next;
-			}
-			return out;
+	  while(first !=  nullptr){
+	    Node<T>* aux = first;
+	    first = first->next;
+	    delete aux;
+	  }
+
+	}
+	
+	T operator[](int pos){
+
+	  int i;
+	  Node<T>* aux = first;
+	  if(pos<0 || pos> n-1){
+	    throw std::out_of_range("Posición fuera del rango");
+	  }
+	  else{
+	  for(i = 0;i<pos;i++){
+		  aux = aux->next;
 		}
+		return aux->data;
+	}
+	}
+	  
+	  friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
+
+	    int i;
+	    Node<T>* aux = list.first;
 	    
+	    for(i = 0; i < list.n; i++){
+	      out<<aux->data;	
+	      out<<" ";
+	      aux = aux->next;
+	    }
+	    return out;
+	  }
 
-		void insert(int pos, T e) override final{
-			if( pos < 0 || pos > n){
-                        	throw std::out_of_range("La posicion no es valida");
-               		 }
-			
-			if (pos == 0){
-				first = new Node<T>(e,first);
-				n++;
-			}
-			else {
-				Node<T> * prev = first;
-				int i = 0;
-				while ( prev!= nullptr && i < pos-1){
-					prev = prev->next;
-					i++;
-				}
-				if(prev != nullptr) {
-					prev->next = new Node<T>(e, prev->next);
-					n++;
-				}
-			}
-		}
 
-		void append(T e) override final {
-			Node<T>* aux = first;
-			int i = 0;
-			while (aux != nullptr && i < n-1){
-				aux = aux->next;
-				i++;
-			}
-			aux->next = new Node<T>(e,nullptr);
-			n++;
-	   	}
+	  void insert(int pos, T e) override{
+	    
+	    Node<T>* nuevoNodo = new Node<T>(e);
+	    if (pos < 0 || pos > n) {
+	      throw std::out_of_range("Posición fuera del rango");
+	    }
+	    if(pos == 0){
+	      prepend(e);
+	    }
+	    else if(pos == n){
+	      append(e);
+	    }
+	    else{
+	      
+	      Node<T>* aux1 = first;
+	      Node<T>* aux2 = first->next;
+	      
+	      for(int i = 1; i < pos; i++){
+		aux1 = aux2;
+		aux2 = aux2->next;
+	      }
+	      aux1->next = nuevoNodo;
+	      nuevoNodo->next = aux2;
+	      n++;
+	    }
+	  }
 
-		void prepend(T e) override final {
-			 first= new Node<T>(e, first);
-			 n++;
-		}
+	  
+  void append(T e) override{
+	    Node<T>* aux = first;
+	    Node<T>* nuevoNodo = new Node<T>(e);
+	    if(aux == nullptr){
+	      first = nuevoNodo;
+	    }
+	    else{
+	      while (aux->next != nullptr) {
+	      aux = aux->next;
+	      }
+	      aux->next = nuevoNodo;
+	    }
+	    n++;
+	  }
+	    
+       
+	  void prepend(T e) override{
+	    
+	    Node<T>* nuevoNodo = new Node<T>(e);
+	  
+	    if (first == nullptr) {
+	      first = nuevoNodo;
+	      nuevoNodo->next = nullptr;
+	    } else {
+       
+	      nuevoNodo->next = first;
+	      first = nuevoNodo;
+	    }
 
-		T remove(int pos) override final {
-			if(pos <  0 || pos > n-1){
-                                throw std::out_of_range("La posicion no es valida");
-                        }
-			Node<T>*prev = nullptr;
-			Node<T>*aux = first;
-			int i = 0;
-			while (aux!= nullptr && i<pos){
-				prev = aux;
-				aux = aux->next;
-				i++;
-			}
-			if (aux != nullptr){
-				if (aux == first){ 
-					first = first ->next;
-				}
-				else {
-					prev->next = aux->next;
-				}
-				T elem = aux->data;
-				delete aux;
-				n--;
-				return elem;
-	    		}
-		}
-            
-		T get(int pos) override final {
-			if(pos <  0 || pos > n-1){
-                                throw std::out_of_range("La posicion no es valida");
-               		}
-			
-			Node<T>*aux = first;
-			int i = 0;
-			while (aux != nullptr && i < pos){
-				aux = aux -> next;
-				i++;
-		 	}
-			T elem = aux->data;
-			return elem;			
-		}
+	    n++;
+	  }
+	  
+	  T remove(int pos) override{
+	    if (pos < 0 || pos >= n) {
+	      throw std::out_of_range("Posición fuera del rango");
+	    }
+	    T m;
+	    Node<T>* aux1 = first;
+	    Node<T>* aux2 = first->next;
+	    
+	    if(pos == 0){
+	      m = first->data;
+	      first = aux2;
+	      delete aux1;
+	    }
+	    else{
+	      for (int i = 1; i < pos; i++) {
+		aux1 = aux2;
+		aux2 = aux2->next;
+	      }
 
-            	int search(T e) override final {
-			Node<T>*aux = first;
-			int i = 0;
-			while (aux!=nullptr && aux-> data != e){
-				aux = aux -> next;
-				i++;
-			}
-			if ( aux != nullptr){ 
-				return i;
-			}
-			else{
-				return -1;
-			}
-		}
+	      aux1->next = aux2->next;
+	      m = aux2->data;
+	      delete aux2;
+	    }
+	    n--;
+	    return m;
+	  }
+  
+	  T get(int pos) override{
 
-		bool empty() override final {
-			return n==0;
-		}
-            
-		int size() override final {
-			return n;
-		}
+	    if (pos < 0 || pos >= n) {
+	      throw std::out_of_range("Posición fuera del rango");
+	    }
+	    Node<T>* aux = first;
+	    int i = 0;
+
+	    while (aux != nullptr && i < pos) {
+	      aux = aux->next;
+	      i++;
+	    }
+	    return aux->data;
+	  
+	  }
+	  
+	  int search(T e) override{
+	     Node<T>* aux = first;
+	     int i = 0;
+	     
+	     while(aux!= nullptr && aux->data != e){
+	       aux = aux->next;
+	       i++;
+	     }
+	     if(aux != nullptr){
+	       return i;
+	     }
+	     else{
+	       return -1;
+	     }
+	     
+	  }
+	  
+	  int size() override{
+	    return n;
+	  }
+	  bool empty() override{
+	    return n == 0;
+	  }
+
+	
 
 };
--- Makefile
+++ Makefile
@@ -7,6 +7,10 @@ bin/testNode: testNode.cpp Node.h
 	mkdir -p bin
 	g++ -o bin/testNode testNode.cpp Node.h
 
+bin/test_ListLinked: test_ListLinked.cpp ListLinked.h List.h
+	mkdir -p bin
+	g++ -o bin/test_ListLinked test_ListLinked.cpp ListLinked.h
+
 clean:
 	rm -r *.o *.gch bin
 
